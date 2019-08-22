f1f74965f4bcf84b1f0f704986d18dbcc9a4d25a
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-08-22 16:32:39 +0800

    Wrote test for checking completeness of scraped water outage details

--- scrapers/tests/test_dcwd_spider.py
+++ scrapers/tests/test_dcwd_spider.py
@@ -57,3 +57,25 @@ class DcwdParserTests(ScraperTestCase):
             self.assertIsNotNone(item.get('scraped_on'))
             self.assertEqual(item.get('provider'), 'DCWD')
             self.assertEqual(item.get('service'), 'Water')
+
+
+    def test_water_outage_details(self):
+        """Tests if scraped outage details are complete."""
+        # Get results from parse_page
+        valid_results = self.get_parse_results(
+            parse_method_name='parse_page',
+            response=make_response_object(
+                filepath=dcwd_details_file,
+                meta={'urgency': 'a', 'title': 'Some Title',
+                      'notice_id': '123'}
+            )
+        )        
+
+        for item in valid_results:
+            # Unpack list of outage details (dicts)
+            details_per_set = item['details']
+            for outage_set in details_per_set:
+                self.assertIsNotNone(outage_set.get('set_n'))
+                self.assertIsNotNone(outage_set.get('when'))
+                self.assertIsNotNone(outage_set.get('where'))
+                self.assertIsNotNone(outage_set.get('why'))
