e72d004e449b7bb0b8feb7b50e4ea9b1c96f8e95
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-08-31 09:18:45 +0800

    Moved actual email sending logic to email_alerts/utils.py

--- email_alerts/tasks.py
+++ email_alerts/tasks.py
@@ -1,27 +1,21 @@
 import logging
 
 from celery import shared_task, task
-from django.core.mail import EmailMessage
 from django.utils import timezone
 
 from email_alerts.models import TransactionalEmail
+from email_alerts.utils import send_email
 
 
 @shared_task(bind=True, max_retries=6)
 def process_and_send_email(self, email_id):
     try:
         email_obj = TransactionalEmail.objects.get(pk=email_id)
+        send_email(email_obj)
+        email_obj.date_sent = timezone.now()
+        email_obj.save()
     except TransactionalEmail.DoesNotExist:
         self.retry(countdown= 2 ** self.request.retries)
-
-    msg = EmailMessage(
-        subject=email_obj.subject_line,
-        body=email_obj.message_body,
-        to=[email_obj.recipient.email]
-    )
-    msg.send()
-    email_obj.date_sent = timezone.now()
-    email_obj.save()
 
 
 @task(name='prepare-send-alerts')
--- email_alerts/utils.py
+++ email_alerts/utils.py
@@ -1,1 +1,11 @@
-# utilities and helpers to be defined later
+from django.core.mail import EmailMessage
+
+def send_email(email_obj):
+    """Sends transactional email."""
+    msg = EmailMessage(
+        subject=email_obj.subject_line,
+        body=email_obj.message_body,
+        to=[email_obj.recipient.email]
+    )
+    msg.send()
+    
